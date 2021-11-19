eca3b15301cf7f417c9cdf499c439bc5b9173edb
Олег <devjatkin.oleg@rambler.ru>
2021-11-19 11:43:45 +0300

    added snowfall color

---
+++ SnowFlakes/SnowFlakes/SnowfallColor.cs
@@ -0,0 +1,14 @@
+﻿namespace Ak.Wpf.SnowFlakes
+{
+    /// <summary>
+    /// Snowfall color
+    /// </summary>
+    public enum SnowfallColor
+    {
+        Default = 0,
+        White = 1,
+        Blue = 2,
+        Gray = 3,
+        Mix = 4
+    }
+}
