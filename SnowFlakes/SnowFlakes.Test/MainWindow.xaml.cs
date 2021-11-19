0983a78c3f7e037669c87cb61b757fa9355f3105
Олег <devjatkin.oleg@rambler.ru>
2021-11-19 11:51:16 +0300

    added examples for colored snowfall

--- SnowFlakes/SnowFlakes.Test/MainWindow.xaml.cs
+++ SnowFlakes/SnowFlakes.Test/MainWindow.xaml.cs
@@ -11,7 +11,29 @@ namespace SnowFlakes.Test
         public MainWindow()
         {
             InitializeComponent();
-            Snowfall snow = new Snowfall(DrawingCanvas);
+
+            //1. Default instance
+            //Snowfall snow = new Snowfall(DrawingCanvas);
+            //snow.Start();
+
+            //2. SnowfallColor.Default
+            //Snowfall snow = Snowfall.Create(DrawingCanvas, SnowfallColor.Default);
+            //snow.Start();
+
+            //3. SnowfallColor.White
+            //Snowfall snow = Snowfall.Create(DrawingCanvas, SnowfallColor.White);
+            //snow.Start();
+
+            //4. SnowfallColor.Blue
+            //Snowfall snow = Snowfall.Create(DrawingCanvas, SnowfallColor.Blue);
+            //snow.Start();
+
+            //4. SnowfallColor.Gray
+            //Snowfall snow = Snowfall.Create(DrawingCanvas, SnowfallColor.Gray);
+            //snow.Start();
+
+            //5. SnowfallColor.Mix
+            Snowfall snow = Snowfall.Create(DrawingCanvas, SnowfallColor.Mix);
             snow.Start();
         }
     }
